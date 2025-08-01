d6c0234feb41e8ed14a7e1c950e8fbfd6a23bb4f
Tijani Abagaro <tijani.a.abagaro@gmail.com>
2025-08-01 17:56:28 -0500

    w card

--- flask_app/app.py
+++ flask_app/app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def home():
     return "Hello from Flask!"
 
+@app.route("/<path:path>", methods=["GET", "POST", "PUT", "DELETE"])
+def catch_all(path):
+    return f"Catch-all path: {path}", 200
+
+
 @app.route("/hello")
 def hello():
     return "Welcome to the /hello endpoint"

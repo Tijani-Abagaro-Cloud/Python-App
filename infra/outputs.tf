1190bd8ee79d2027fea73ce78b9f22cd43376b60
Tijani Abagaro <tijani.a.abagaro@gmail.com>
2025-08-03 00:31:15 -0500

    duplication resolution output

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -7,10 +7,3 @@ output "api_gateway_stage_invoke_url" {
   description = "Invoke URL for the deployed API stage"
   value       = aws_api_gateway_stage.dev.invoke_url
 }
-output "hello_resource_id" {
-  value = local.hello_resource_id
-}
-
-output "api_gateway_stage_invoke_url" {
-  value = "https://${var.rest_api_id}.execute-api.${var.region}.amazonaws.com/dev/hello"
-}

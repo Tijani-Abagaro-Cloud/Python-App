87de3c1f821193f516d932bf33e0e54f32493198
Tijani Abagaro <tijani.a.abagaro@gmail.com>
2025-08-03 01:25:24 -0500

    deploy second

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,7 +3,3 @@ output "hello_resource_id" {
   value       = local.hello_resource_id
 }
 
-output "api_gateway_stage_invoke_url" {
-  description = "Invoke URL for the deployed API stage"
-  value       = aws_api_gateway_stage.dev.invoke_url
-}

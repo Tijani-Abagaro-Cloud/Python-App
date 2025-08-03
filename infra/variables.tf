744cd75cd6761289edd8400ae7d15d348692ca57
Tijani Abagaro <tijani.a.abagaro@gmail.com>
2025-08-03 01:47:39 -0500

    minor change

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,11 +1,22 @@
 variable "region" {}
+
 variable "rest_api_id" {}
+
 variable "parent_resource_id" {}
+
 variable "hello_resource_id" {
   default = ""
 }
+
 variable "vpc_link_id" {}
+
 variable "nlb_name" {}
+
 variable "web_acl_arn" {
   default = null
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC where resources will be deployed"
+  type        = string
 }

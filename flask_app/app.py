ee70932e8ac3b4e27c96252425effd69ea5f3153
Tijani Abagaro <tijani.a.abagaro@gmail.com>
2025-08-03 15:38:55 -0500

    jsonfy

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,18 +1,18 @@
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return Response("Hello from Flask!", status=200, mimetype="text/plain")
+    return jsonify(message="Hello from Flask!"), 200
 
 @app.route("/hello")
 def hello():
-    return Response("Welcome to the /hello endpoint", status=200, mimetype="text/plain")
+    return jsonify(message="Welcome to the /hello endpoint"), 200
 
 @app.route("/status")
 def status():
-    return jsonify({"status": "OK", "env": "dev"})
+    return jsonify(status="OK", env="dev"), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
